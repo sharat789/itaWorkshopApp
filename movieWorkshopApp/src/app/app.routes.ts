63cb716951e172774b867ab31611a6d8a48996e1
sharat789 <sharat157@gmail.com>
2024-03-28 23:10:14 +0200

    Task4_S

--- movieWorkshopApp/src/app/app.routes.ts
+++ movieWorkshopApp/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/movies', pathMatch: 'full'},
+    {path: 'movies', component: MovieListComponent},
+    {path: '**', component: NotFoundComponent}
+];
