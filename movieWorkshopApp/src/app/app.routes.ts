c34db5b1a2129d7a1dcba2d2007907044a7209e3
Sharat Nair <sharat.nair@swedbank.lt>
2024-03-29 11:34:37 +0200

    Task6_S

--- movieWorkshopApp/src/app/app.routes.ts
+++ movieWorkshopApp/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-    {path: 'movies', component: MovieListComponent},
-    {path: 'movies/movie/:id', component: MovieDetailsComponent},
-    {path: '', redirectTo: '/movies', pathMatch: 'full'},
-    {path: '**', component: NotFoundComponent}
+  { path: 'movies', component: MovieListComponent },
+  { path: 'movies/movie/:id', component: MovieDetailsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
 ];
---
+++ movieWorkshopApp/src/app/components/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      username: [''],
+      password: [''],
+    });
+  }
+
+  onSubmit() {
+    console.log('asfas');
+    const username = this.loginForm.value.username;
+    const password = this.loginForm.value.password;
+    if (this.authService.login(username, password)) {
+      alert('Login successful');
+      this.router.navigate(['/movies']);
+    } else {
+      alert('Login failed');
+    }
+  }
+}
